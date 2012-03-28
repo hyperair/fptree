e8e1bcd60fdf77d84388449c3f20ec20543d80b8
Chow Loong Jin <hyperair@debian.org>
2012-03-28 16:17:31 +0800

    Add main() function

---
+++ src/main.cc
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <min_support>"
+                  << std::endl;
+
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    double min_support = std::atof (argv[2]);
+
+    fpt::transaction_database db = fpt::parse_file (filename);
+    fpt::itemset sorted_items = db.extract_itemset ();
+
+    sorted_items.prune (min_support);
+
+    fpt::fptree tree (items, db);
+
+    std::cout << "File: " << filename << std::endl
+              << "Number of nodes: " << tree.size () << std::endl
+              << "Number of leaves:" << tree.n_leaves () << std::endl
+              << "Height: " << tree.height () << std::endl;
+
+    fpt::fptree::fanout_stats stats = tree.fanout_stats ();
+
+    std::cout << "Minimum fanout: " << stats.min << std::endl
+              << "Average fanout: " << stats.average << std::endl
+              << "Maximum fanout: " << stats.max << std::endl
+
+    return 0;
+}
